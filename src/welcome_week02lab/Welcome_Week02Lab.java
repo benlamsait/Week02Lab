2d2286bcf9743fb07b46c3da4802cb6530cb73ab
Ben Lam <Ben Lam@benlam-predator>
2023-01-21 12:16:49 -0700

    New Project Creation Create a new Java Application for HelloWorld.

---
+++ src/welcome_week02lab/Welcome_Week02Lab.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package welcome_week02lab;
+
+/**
+ *
+ * @author Ben Lam
+ */
+public class Welcome_Week02Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Welcome Week02Lab");
+    }
+    
+}
